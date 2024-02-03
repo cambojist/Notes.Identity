e3f55817dde8f58d00911169f9a8a65dc18aee22
Vitalii Kharchenko <sir.vetal98@gmail.com>
2024-02-03 14:46:26 +0200

    Configure IdentityServer

--- Notes.Identity/Program.cs
+++ Notes.Identity/Program.cs
@@ -1,6 +1,62 @@
+using IdentityModel;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetValue<string>("DbConnection");
+
+builder.Services.AddDbContext<AuthDbContext>(opt =>
+{
+    opt.UseSqlite(connectionString);
+});
+builder.Services.AddIdentity<AppUser, IdentityRole>(cfg =>
+{
+    cfg.Password.RequiredLength = 4;
+    cfg.Password.RequireDigit = false;
+    cfg.Password.RequireNonAlphanumeric = false;
+    cfg.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureApplicationCookie(cfg =>
+{
+    cfg.Cookie.Name = "Notes.Identity.Cookie";
+    cfg.LoginPath = "/Auth/Login";
+    cfg.LogoutPath = "/Auth/Logout";
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(e, "An error occurred while app initialization");
+    }
+}
 
+app.UseRouting();
+app.UseIdentityServer();
+app.MapGet("/", () => "Hello World!");
 app.Run();
